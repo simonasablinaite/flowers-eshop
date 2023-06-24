9ad49548940434cf1e77164d09e74a4c72c3ef8b
Simona <simonasablinaite@gmail.com>
2023-06-24 08:32:19 +0300

    SASS, ROUTES

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import './App.css';
+import './style/home.css';
+
+import { Routes, Route, NavLink, Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        Labas rytas!🌼
+      <header className='header'>
+        <nav className='navigation'>
+          <NavLink className='nav-item' to='/'>Home Page</NavLink>
+        </nav>
       </header>
+
+      <Routes>
+
+
+
+
+
+
+        <Route path='*' element={<><h1>404 Error. Page Not Found</h1>  <Link to='/'>Go to Home Page</Link> </>} />
+      </Routes>
+
     </div>
   );
 }
